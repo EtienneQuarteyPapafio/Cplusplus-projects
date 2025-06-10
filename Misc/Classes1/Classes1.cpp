705e06aa18003e9b0b6faea3a2d9ffdf7b163d90
EtienneQuarteyPapafio <etiennequartey@gmail.com>
2025-06-10 20:56:54 +0100

    Classes
    Beginning work on classes

---
+++ Misc/Classes1/Classes1.cpp
@@ -0,0 +1,21 @@
+// Classes1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//Classes are like structs however they can store both data and functions
+//Data members are daata variables
+//Member functions are functions used to manipulate these variables
+
+#include <iostream>
+using namespace std;
+
+class products //class keyword, name of class
+{
+	int price;
+
+}apple,melon,grapes;//object names
+
+
+int main()
+{
+ 
+}
+
