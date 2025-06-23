6d077867909c1ab860348f62704acb3d6e8372e5
EtienneQuarteyPapafio <etiennequartey@gmail.com>
2025-06-23 19:56:47 +0100

    OSc

---
+++ Oscillator/Oscillator.cpp
@@ -0,0 +1,17 @@
+// Oscillator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//This program creates an Oscillator for use in DSP
+
+//A sinusoid is expressed as x(t)=Amplitude*Cosine(radian*time+initial phase)
+
+//x(t)=Acos(2*pi*frequency+phase)
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+  
+    std::cout << "Hello World!\n";
+}
+
