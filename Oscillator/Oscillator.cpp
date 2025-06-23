f23f9eea632c888302b34f548b7087ed0a4af0bd
EtienneQuarteyPapafio <etiennequartey@gmail.com>
2025-06-24 00:53:59 +0100

    Removed files

--- Oscillator/Oscillator.cpp
+++ Oscillator/Oscillator.cpp
@@ -7,11 +7,45 @@
 //x(t)=Acos(2*pi*frequency+phase)
 
 #include <iostream>
-#include <math.h>
+#include <cmath>
+#include <array>
 
-int main()
-{
-  
-    std::cout << "Hello World!\n";
+# define M_PI           3.14159265358979323846  /* pi */
+using namespace std;
+
+class SineOsc {
+public:
+	SineOsc(double sampleRate, double amplitude)
+	{
+		mTs = 1 / sampleRate;
+		mAmp = amplitude;
+	}
+	double process(double freq) {
+		double newPhase = mPhase + freq * 2 * M_PI * mTs;
+		mPhase = fmod(newPhase, 2 * M_PI);
+		return mAmp * cos(mPhase);
+	}
+private:
+	double mTs;
+	double mAmp;
+	double mPhase;
+};
+
+int main() {
+	const int N = 44100; // size
+	const double Ts = 1 / 44100.0; // sampling period
+	double freq = 100; // frequency
+	const double A = 0.5; // amplitude
+
+	double Fs = freq;
+
+	std::array<double, N> wave;
+
+	SineOsc sine(freq, A);
+
+	for (int i = 0; i < N; i++) {
+		wave[i] = sine.process(freq);
+		freq = freq + 0.1;
+	}
+
 }
-
