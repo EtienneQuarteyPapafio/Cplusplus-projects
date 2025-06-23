8578ae503310bdb437788a19d01cb85b14d08e7b
EtienneQuarteyPapafio <etiennequartey@gmail.com>
2025-06-23 18:00:26 +0100

    Update Classes1.cpp

--- Misc/Classes1/Classes1.cpp
+++ Misc/Classes1/Classes1.cpp
@@ -7,15 +7,31 @@
 #include <iostream>
 using namespace std;
 
-class products //class keyword, name of class
+class CRectangle //class keyword, name of class
 {
-	int price;
+	int x, y; //member 1 and 2 variables private by default
+public:
+	void set_values(int, int);//functions
+	int area()
+	{
+		return (x * y);
+	}
 
-}apple,melon,grapes;//object names
+}; //object realted to class
 
+
+void CRectangle::set_values(int a, int b)
+{
+	x = a;
+	y = b;
+}
 
 int main()
 {
- 
+	CRectangle rect; //creates an object with the name rect
+	rect.set_values(3, 4);
+	cout << "area: " << rect.area();
+	return 0;
+
 }
 
